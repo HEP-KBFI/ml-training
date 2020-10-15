a7fa68b52d265d4964e714487e7e906d10317cc8
Laurits7 <laurits.tani@gmail.com>
2020-10-15 17:08:26 +0300

    .

--- python/hh_data_tools.py
+++ python/hh_data_tools.py
@@ -13,6 +13,7 @@ def set_signal_sample_info(bdt_type, folder_name, masses):
         '2b2v': 'bbvv'
     }
     for key in name_map:
+        sample_name = folder_name.replace(key, name_map[key])
          if 'node' in sample_name:
              sample_name = sample_name.replace('_node', '')
              node_type = sample_name.split('_')[3]
