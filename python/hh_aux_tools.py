1c0b68c0954d24451f861680dae174d788d37837
Laurits7 <laurits.tani@gmail.com>
2020-10-26 15:53:47 +0200

    trainvar_info

--- python/hh_aux_tools.py
+++ python/hh_aux_tools.py
@@ -1531,11 +1531,14 @@ def get_hh_parameters(
             parameters['trainvars'].append(str(info['key']))
     all_trainvars_path = os.path.join(channel_dir, 'all_trainvars.json')
     all_trainvar_info = dlt.read_trainvar_info(all_trainvars_path)
-    parameters['trainvars_info'] = []
-    with open(all_trainvars_path, 'rt') as infile:
+    parameters['trainvars'] = []
+    with open(trainvars_path, 'rt') as infile:
         for line in infile:
             info = json.loads(line)
-            parameters['trainvars_info'].append(str(info['key']))
+            parameters['trainvars'].append(str(info['key']))
+    all_trainvars_path = os.path.join(channel_dir, 'all_trainvars.json')
+    all_trainvar_info = dlt.read_trainvar_info(all_trainvars_path)
+    parameters['trainvar_info'] = all_trainvar_info
     parameters.update(info_dict)
     return parameters
 
