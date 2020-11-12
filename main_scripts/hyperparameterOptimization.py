30f96e93127245dfaa46efde4fd8c6ea0448688b
Laurits Tani <30724920+Laurits7@users.noreply.github.com>
2020-11-12 15:46:13 +0200

    Data from /scratch-persistent (#74)
    * copy ntuples to scratch and load from there in order to speed up data loading
    
    * data from scratch without intermediate check
    
    * always check if paths are on /scratch
    
    * use /scratch-persistent instead of /scratch

--- main_scripts/hyperparameterOptimization.py
+++ main_scripts/hyperparameterOptimization.py
@@ -12,6 +12,7 @@ Options:
 import os
 import numpy as np
 import docopt
+import subprocess
 from machineLearning.machineLearning import slurm_tools as st
 from machineLearning.machineLearning import pso_tools as pt
 from machineLearning.machineLearning import universal_tools as ut
@@ -35,6 +36,7 @@ def main(to_continue, opt_dir):
     if not to_continue:
         ut.save_run_settings(output_dir)
         ut.save_info_dir(output_dir)
+    use_scratch_for_data(global_settings)
     print("::::::: Reading parameters :::::::")
     param_file = os.path.join(
         settings_dir,
@@ -54,6 +56,49 @@ def main(to_continue, opt_dir):
         output_dir, 'best_hyperparameters.json')
     ut.save_dict_to_json(optimal_hyperparameters, best_parameters_path)
     print("Results saved to " + str(output_dir))
+
+
+def use_scratch_for_data(global_settings):
+    renew_data_paths(global_settings)
+    original_paths = get_original_input_paths(global_settings)
+    SCRATCH_DIR = '/scratch-persistent'
+    for key in original_paths:
+        era_dir = original_paths[key]
+        wildcard = os.path.join(era_dir, '*', 'hadd*.root')
+        print('rsyncing ' + str(key) + ' paths ')
+        subprocess.call(['rsync', '-aRv', era_dir, SCRATCH_DIR])
+
+
+def get_original_input_paths(global_settings):
+    info_dir = os.path.join(
+        os.path.expandvars('$CMSSW_BASE'),
+        'src/machineLearning/machineLearning/info/HH'
+    )
+    addition = ut.create_infoPath_addition(global_settings)
+    info_file = os.path.join(
+        info_dir, global_settings['channel'], addition, 'info.json')
+    info_dict = ut.read_json_cfg(info_file)
+    paths = info_dict['tauID_training'][global_settings['tauID_training']]
+    for key in paths.keys():
+        path = paths[key]
+        paths[key] = path.replace(
+            path.split('/hhAnalysis')[0], path.split('/hhAnalysis')[0] + '/.')
+    return paths
+
+
+def renew_data_paths(global_settings):
+    addition = ut.create_infoPath_addition(global_settings)
+    channel_dir = os.path.expandvars(
+        os.path.join(global_settings['output_dir'], 'run_info')
+    )
+    info_file = os.path.join(channel_dir, addition, 'info.json')
+    info_dict = ut.read_json_cfg(info_file)
+    paths = info_dict['tauID_training'][global_settings['tauID_training']]
+    for key in paths:
+        path = paths[key]
+        paths[key] = path.replace(
+            path.split('/hhAnalysis')[0], '/scratch-persistent/')
+    ut.save_dict_to_json(info_dict, info_file)
 
 
 if __name__ == '__main__':
