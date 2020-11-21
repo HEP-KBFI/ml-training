a15e07870e00e113ce63c4f72f7f2029701f961c
saswatinandan <saswati.nandan@cern.ch>
2020-11-21 12:15:50 +0200

    define list of particles

---
+++ python/particle_list.py
@@ -0,0 +1,5 @@
+def ll_objects_list() :
+    low_level_object = {}
+    low_level_object['bb1l'] = ["bjet1", "bjet2", "wjet1", "wjet2", "lep"]
+    low_level_object['bb2l'] = ["bjet1", "bjet2", "lep1", "lep2"]
+    return low_level_object
