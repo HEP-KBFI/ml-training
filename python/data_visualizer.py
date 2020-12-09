6eacba724a21370df307b07bece5d879ea5687f2
Laurits7 <laurits.tani@gmail.com>
2020-12-09 16:49:07 +0200

    typo

--- python/data_visualizer.py
+++ python/data_visualizer.py
@@ -98,7 +98,7 @@ class DataVisualizer(object):
             self.plot_single_mode_correlation(
                 mode_data, output_dir, self.mapping[class_])
         self.plot_single_mode_correlation(
-            self.data, trainvars, output_dir, 'total')
+            self.data, output_dir, 'total')
 
     def plot_single_mode_correlation(self, data, output_dir, addition):
         """ Creates the correlation matrix for one specific target or for
