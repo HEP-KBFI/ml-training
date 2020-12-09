ec3987a4c6fa65a558fe019c7cc9d6a7db9fa156
Laurits7 <laurits.tani@gmail.com>
2020-12-09 16:52:39 +0200

    remove newline

--- python/data_visualizer.py
+++ python/data_visualizer.py
@@ -97,8 +97,7 @@ class DataVisualizer(object):
             mode_data = self.data.loc[self.data['target'] == class_]
             self.plot_single_mode_correlation(
                 mode_data, output_dir, self.mapping[class_])
-        self.plot_single_mode_correlation(
-            self.data, output_dir, 'total')
+        self.plot_single_mode_correlation(self.data, output_dir, 'total')
 
     def plot_single_mode_correlation(self, data, output_dir, addition):
         """ Creates the correlation matrix for one specific target or for
