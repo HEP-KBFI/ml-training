1e75e538fe57fdde113642ddddefbfeae74a008e
Laurits7 <laurits.tani@gmail.com>
2020-12-19 08:28:27 +0200

    fix bbWW indentation issues

--- python/bbWW_tools.py
+++ python/bbWW_tools.py
@@ -94,59 +94,59 @@ class bbWWLoader(HHDataLoader):
         return data
 
     def get_ntuple_paths(self, input_path, folder_name, file_type='hadd*Tight.root'):
-       paths = []
-       background_catfile = os.path.join(
-            os.path.expandvars('$CMSSW_BASE'),
+        paths = []
+        background_catfile = os.path.join(
+             os.path.expandvars('$CMSSW_BASE'),
             'src/machineLearning/machineLearning/info',
             'HH',
             'background_categories.json'
         )
-       background_categories = ut.read_json_cfg(background_catfile)
-       if 'signal' not in folder_name and folder_name in background_categories.keys():
-           bkg_elements = background_categories[folder_name]
-           for bkg_element in bkg_elements:
-               bkg_element_paths = self.find_paths_both_conventions(
-                   input_path, bkg_element, file_type=file_type)
-               print('--------------')
-               print(bkg_element)
-               print(bkg_element_paths)
-               paths.extend(bkg_element_paths)
-       else:
-           paths = self.find_paths_both_conventions(
-               input_path, folder_name + '*', file_type=file_type)
-       return paths
+        background_categories = ut.read_json_cfg(background_catfile)
+        if 'signal' not in folder_name and folder_name in background_categories.keys():
+            bkg_elements = background_categories[folder_name]
+            for bkg_element in bkg_elements:
+                bkg_element_paths = self.find_paths_both_conventions(
+                    input_path, bkg_element, file_type=file_type)
+                print('--------------')
+                print(bkg_element)
+                print(bkg_element_paths)
+                paths.extend(bkg_element_paths)
+        else:
+            paths = self.find_paths_both_conventions(
+                input_path, folder_name + '*', file_type=file_type)
+        return paths
 
     def do_loading(self):
-      eras = self.preferences['included_eras']
-      data = pandas.DataFrame({})
-      for era in eras:
-         input_path_key = 'inputPath' + str(era)
-         self.preferences['era_inputPath'] = self.preferences[input_path_key]
-         self.preferences['era_keys'] = self.preferences['keys' + str(era)]
-         self.print_info()
-         era_data = self.load_data_from_one_era()
-         era_data['era'] = era
-         data = data.append(era_data, ignore_index=True, sort=False)
-      if self.global_settings['dataCuts'] != 0:
-         total_data = self.data_cutting(data)
-      TT = total_data.loc[total_data["process"] == "TT"].head(200000)
-      ST = total_data.loc[total_data["process"] == "ST"].head(200000)
-      Other = total_data.loc[total_data["process"] == "Other"].head(200000)
-      W = total_data.loc[total_data["process"] == "W"].head(200000)
-      DY = total_data.loc[total_data["process"] == "DY"].head(200000)
-      HH = total_data.loc[total_data["target"] == 1]
-      alldata = [TT, ST, Other, W, DY, HH]
-      total_data = pandas.concat(alldata)
-      print("DY: ", len(total_data.loc[total_data["process"] == "DY"]),\
+        eras = self.preferences['included_eras']
+        data = pandas.DataFrame({})
+        for era in eras:
+            input_path_key = 'inputPath' + str(era)
+            self.preferences['era_inputPath'] = self.preferences[input_path_key]
+            self.preferences['era_keys'] = self.preferences['keys' + str(era)]
+            self.print_info()
+            era_data = self.load_data_from_one_era()
+            era_data['era'] = era
+            data = data.append(era_data, ignore_index=True, sort=False)
+        if self.global_settings['dataCuts'] != 0:
+            total_data = self.data_cutting(data)
+        TT = total_data.loc[total_data["process"] == "TT"].head(200000)
+        ST = total_data.loc[total_data["process"] == "ST"].head(200000)
+        Other = total_data.loc[total_data["process"] == "Other"].head(200000)
+        W = total_data.loc[total_data["process"] == "W"].head(200000)
+        DY = total_data.loc[total_data["process"] == "DY"].head(200000)
+        HH = total_data.loc[total_data["target"] == 1]
+        alldata = [TT, ST, Other, W, DY, HH]
+        total_data = pandas.concat(alldata)
+        print(
+            "DY: ", len(total_data.loc[total_data["process"] == "DY"]),\
             "W: ", len(total_data.loc[total_data["process"] == "W"]), \
             "TT: ", len(total_data.loc[total_data["process"] == "TT"]), \
             'ST: ', len(total_data.loc[total_data["process"] == "ST"]), \
             'Other:', len(total_data.loc[total_data["process"] == "Other"]), \
             'HH', len(total_data.loc[total_data["target"] == 1])
-         )
-
-      self.print_nr_signal_bkg(total_data)
-      return total_data
+        )
+        self.print_nr_signal_bkg(total_data)
+        return total_data
 
     def load_data_from_tfile(
         self,
@@ -157,7 +157,7 @@ class bbWWLoader(HHDataLoader):
         input_tree
     ):
         if 'TT' in folder_name:
-          self.nr_events_per_file = 3000000 
+            self.nr_events_per_file = 3000000
         return dlt.load_data_from_tfile(
             self,
             process,
@@ -165,12 +165,13 @@ class bbWWLoader(HHDataLoader):
             target,
             path,
             input_tree
-          )
+        )
 
     def set_background_sample_info(self, path):
-      if 'ST' in path: return 'ST', 0
-      else:
-        return HHDataLoader.set_background_sample_info(self, path)
+        if 'ST' in path:
+            return 'ST', 0
+        else:
+            return HHDataLoader.set_background_sample_info(self, path)
 
     def set_signal_sample_info(self, folder_name):
         target = 1
@@ -181,29 +182,31 @@ class bbWWLoader(HHDataLoader):
         else:
             process = folder_name.replace('_' + folder_name.split('_')[-1], '')
         return process, target
+
     def nonresonant_data_imputer(
             self, chunk_df, folder_name, target, data
     ):
-       if target == 1 and 'ggf' in folder_name:
+        if target == 1 and 'ggf' in folder_name:
             for i in range(len(self.preferences['nonResScenarios'])):
-               chunk_df_node = chunk_df.copy()
-               scenario = self.preferences['nonResScenarios'][i]
-               chunk_df_node['nodeX'] = i
-               for idx, node in enumerate(self.preferences['nonResScenarios']):
-                 chunk_df_node[node] = 1 if idx == i else 0
-               chunk_df_node['nodeXname'] = scenario
-               if scenario is not "SM":
-                   nodeWeight = chunk_df_node['Weight_' + scenario]
-                   nodeWeight /= chunk_df_node['Weight_SM']
-                   chunk_df_node['totalWeight'] *= nodeWeight
-               data = data.append(chunk_df_node, ignore_index=True, sort=False)
-       else :
-          chunk_df_node = chunk_df.copy()
-          chunk_df_node['nodeXname'] = np.random.choice(self.preferences['nonResScenarios'], size=len(chunk_df_node))
-          for idx, node in enumerate(self.preferences['nonResScenarios']):
-            if len(chunk_df_node.loc[chunk_df_node['nodeXname'] == node]):
-                chunk_df_node.loc[chunk_df_node['nodeXname'] == node, node] = 1
-                chunk_df_node.loc[chunk_df_node['nodeXname'] != node, node] = 0
-                chunk_df_node.loc[chunk_df_node['nodeXname'] == node, 'nodeX'] = idx
+                chunk_df_node = chunk_df.copy()
+                scenario = self.preferences['nonResScenarios'][i]
+                chunk_df_node['nodeX'] = i
+                for idx, node in enumerate(self.preferences['nonResScenarios']):
+                    chunk_df_node[node] = 1 if idx == i else 0
+                chunk_df_node['nodeXname'] = scenario
+                if scenario is not "SM":
+                    nodeWeight = chunk_df_node['Weight_' + scenario]
+                    nodeWeight /= chunk_df_node['Weight_SM']
+                    chunk_df_node['totalWeight'] *= nodeWeight
+                data = data.append(chunk_df_node, ignore_index=True, sort=False)
+        else :
+            chunk_df_node = chunk_df.copy()
+            chunk_df_node['nodeXname'] = np.random.choice(
+                self.preferences['nonResScenarios'], size=len(chunk_df_node))
+            for idx, node in enumerate(self.preferences['nonResScenarios']):
+                if len(chunk_df_node.loc[chunk_df_node['nodeXname'] == node]):
+                    chunk_df_node.loc[chunk_df_node['nodeXname'] == node, node] = 1
+                    chunk_df_node.loc[chunk_df_node['nodeXname'] != node, node] = 0
+                    chunk_df_node.loc[chunk_df_node['nodeXname'] == node, 'nodeX'] = idx
             data = data.append(chunk_df_node, ignore_index=True, sort=False)
-       return data
+        return data
